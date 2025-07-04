1603f3249eca92b6eaf595116eb624fe49d37ef1
Lucas Mendes <lucas.d.mendes@ba.estudante.senai.br>
2025-07-04 10:43:27 -0300

    primeiro commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ComponentesComponent } from './componentes/componentes.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComponentesComponent,
+    CabecalhoComponent,
+    RodapeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
