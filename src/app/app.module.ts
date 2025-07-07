334c12994ae57146e787b4c4813b3107a6d16e87
Lucas Mendes <lucas.d.mendes@ba.estudante.senai.br>
2025-07-07 08:28:53 -0300

    componente login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ComponentesComponent } from './componentes/componentes.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentesComponent,
     CabecalhoComponent,
-    RodapeComponent
+    RodapeComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
