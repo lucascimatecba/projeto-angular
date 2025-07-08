7f46c2b64047efe8548656d518275a9958c95573
Lucas Mendes <lucas.d.mendes@ba.estudante.senai.br>
2025-07-08 10:46:35 -0300

    configurações header fora de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, Event as RouterEvent } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'projeto-angular';
+export class AppComponent implements OnInit {
+  showHeader$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter((event: RouterEvent): event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      const currentUrl = event.urlAfterRedirects;
+      this.showHeader$.next(!currentUrl.includes('/login'));
+    });
+  }
 }
