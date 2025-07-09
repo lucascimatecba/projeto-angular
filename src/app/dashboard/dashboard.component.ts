ad853a13b48afe52181d95224af33ab54a221e1f
Lucas Mendes <lucas.d.mendes@ba.estudante.senai.br>
2025-07-09 11:52:56 -0300

    dados select

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-
+  dados = [
+    {
+      vin: '2FRHDUYS2Y63NHD22454',
+      odometro: '50000 Km',
+      combustivel: '90%',
+      status: 'On',
+      lat: '12.2322',
+      long: '35.2314'
+    }
+  ];
+  displayedColumns: string[] = ['vin', 'odometro', 'combustivel', 'status', 'lat', 'long'];
 }
