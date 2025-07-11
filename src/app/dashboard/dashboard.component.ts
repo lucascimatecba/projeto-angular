8d44580dd596b9eeb51a3f4218dacbb5b0864d1c
Lucas Mendes <lms252006@gmail.com>
2025-07-10 21:41:21 -0300

    objeto cards

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-  dados = [
+  dadosOriginal = [
     {
       vin: '2FRHDUYS2Y63NHD22454',
       odometro: '50000 Km',
@@ -14,7 +14,26 @@ export class DashboardComponent {
       status: 'On',
       lat: '12.2322',
       long: '35.2314'
-    }
+    },
+  ];
+  cards = [
+    { titulo: 'Vendas', valor: 1500 },
+    { titulo: 'Conectados', valor: 500 },
+    { titulo: 'Atualizados', valor: 750 }
   ];
   displayedColumns: string[] = ['vin', 'odometro', 'combustivel', 'status', 'lat', 'long'];
+
+  dados = [...this.dadosOriginal];
+  codigoBusca = '';
+
+  filtrarPorVin() {
+    const termo = this.codigoBusca.toLowerCase();
+    if (!termo) {
+      this.dados = [...this.dadosOriginal];
+    } else {
+      this.dados = this.dadosOriginal.filter((item) =>
+        item.vin.toLowerCase().includes(termo)
+      );
+    }
+  }
 }
