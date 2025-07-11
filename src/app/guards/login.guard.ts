e31bf51776a2a27671e2a1d245d6bb6e9839659c
Lucas Mendes <lucas.d.mendes@ba.estudante.senai.br>
2025-07-11 11:27:21 -0300

    proteção de rotas

--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -2,13 +2,11 @@ import { inject } from '@angular/core';
 import { AuthService } from './../services/auth.service';
 import { CanActivateFn, Router } from '@angular/router';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const loginGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  if(authService.estaLogado()) {
-    return true;
-  } else {
-    return router.createUrlTree(['/login'])
-  }
+  return authService.estaLogado()
+    ? router.createUrlTree(['/home'])
+    : true;
 };
