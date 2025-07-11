ff7655eb40a93731a31f5312f5bb7dae624d53dc
Lucas Mendes <lucas.d.mendes@ba.estudante.senai.br>
2025-07-11 11:54:07 -0300

    integração com api dashboard

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { VeiculosAPI } from '../models/veiculo.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
+  private readonly baseURL = 'http://localhost:3001';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getVehicle(): Observable<VeiculosAPI> {
+    return this.http.get<VeiculosAPI> (`${this.baseURL}/vehicles`);
+  }
 }
