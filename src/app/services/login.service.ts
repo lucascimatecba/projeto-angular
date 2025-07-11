5b129132130dd34091c3c97275589dc6cfb70bf3
Lucas Mendes <lucas.d.mendes@ba.estudante.senai.br>
2025-07-11 10:50:08 -0300

    construção de login

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuario } from '../models/usuario.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  private baseURL = 'http://localhost:3001'
+
+  constructor(private http: HttpClient) { }
+
+  login(email: string, senha: string): Observable<Usuario> {
+    return this.http.post<Usuario>(`${this.baseURL}/login`, {email, senha});
+  }
 }
